bfc5e08209550d4a1951aceb8eb3fcb0148a382d
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-02-23 01:17:21 +0800

    Update ajax.js

--- src/ajax.js
+++ src/ajax.js
@@ -1,9 +1,3 @@
 function LoadSection(url,section,after){
-  $(section).hide()
-  $(section).load(url + ' ' + section,function(data){
-    alert(data)
-    after(data)
-    $(section).show()
-  })
-  history.pushState('',{},url)
+  
 }
